c2022e188909dd3fd363c313946f1059e00f6b7e
Timofey <avetisov.tm@gmail.com>
2021-09-25 10:38:04 +0300

    Some bugs have been fixed

--- main.cpp
+++ main.cpp
@@ -20,15 +20,15 @@ public:
     long long operator%(Point);
     bool operator==(Point);
     bool operator!=(Point);
-    friend ostream operator<<(ostream&, const Point&);
-    long long lenght() { return x * x + y * y; }
+    friend ostream& operator<<(ostream&, const Point&);
+    long long length() { return x * x + y * y; }
 };
 
 Point Point::operator+(Point a) {
     return Point(x + a.x, y + a.y);
 }
 
-Point Point::operator-(Point b) {
+Point Point::operator-(Point a) {
     return Point(x - a.x, y - a.y);
 }
 
@@ -63,15 +63,39 @@ bool Point::operator!=(Point a) {
 }
 
 double get_angle(Point& a, Point& b) {
-    int x1 = a.get_x(), y1 = a.get_y();
-    int x2 = b.get_x(), y2 = b.get_y();
+    long long x1 = a.get_x(), y1 = a.get_y();
+    long long x2 = b.get_x(), y2 = b.get_y();
     return atan2(y2, x2) - atan2(y1, x1);
 }
 
 int main() {
-    Point a;
-    Point b;
-    int ax, ay, bx, by;
-    cout << "Введите координаты первого вектора: ";
-    c
+    long long ax, ay, bx, by;
+    cout << "Enter cord of first vector: ";
+    cin >> ax >> ay;
+    Point a(ax, ay);
+    cout << "Enter cord of second vetor: ";
+    cin >> bx >> by;
+    Point b(bx, by);
+    cout << "Sum of two vectors: " << a + b << endl;
+    cout << "Difference of two vectors: " << a - b << endl;
+    cout << "Enter an integer by which you want to multiply the first vector: ";
+    long long k;
+    cin >> k;
+    cout << "Multiply first vector by a k: " << a * k << endl;
+    cout << "Scalar product of vectors: " << a * b << endl;
+    cout << "Skew product of vectors: " << a % b << endl;
+    cout << "Is the first vector equal to the second vector: ";
+    if (a == b) {
+        cout << "true" << endl;
+    } else {
+        cout << "false" << endl;
+    }
+    cout << "Is the first vector not equal to the second vector: ";
+    if (a != b) {
+        cout << "true" << endl;
+    } else {
+        cout << "false" << endl;
+    }
+    cout << "Square of the length of the first and second vectors: " << a.length() << ' ' << b.length() << endl;
+    cout << "Directional angle from the first vector to second vector: " << get_angle(a, b) << endl;
 }
